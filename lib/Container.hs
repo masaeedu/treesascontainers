d28667b7d6ff3134e27eefec6742997a50eaf679
Asad Saeeduddin <masaeedu@gmail.com>
2020-11-29 22:09:44 -0500

    Cosmetic

--- lib/Container.hs
+++ lib/Container.hs
@@ -1,6 +1,11 @@
 module Container where
 
-data Container (s :: *) (proxy :: s -> *) (p :: s -> *) (a :: *) = forall (x :: s). Container (proxy x) (p x -> a)
+data Container
+  (shape :: *)
+  (singleton :: shape -> *)
+  (position :: shape -> *)
+  (a :: *)
+  = forall (x :: shape). Container (singleton x) (position x -> a)
 
 -- A container is (isomorphic to) a dependent sum of a shape and a function from a shape-appropriate position to values.
 class IsContainer (shape :: *) (singleton :: shape -> *) (position :: shape -> *) (f :: * -> *) | f -> shape singleton position, shape singleton position -> f
